78fd7041fd6eb61e7070f8ac34e186c95bbf3a4b
vinicius <car_silva@hotmail.com>
2021-04-21 19:04:46 -0300

    aula-01

--- projeto_nlw/src/server.ts
+++ projeto_nlw/src/server.ts
@@ -1,5 +1,21 @@
 import express from "express";
+import { request } from "node:https";
 
  const app = express();
 
+/**
+ * GET = Busca
+ * POST = CRIACAO
+ * PUT = ALTERACAO
+ * DELETE = DELETAR
+ * PATCH = ALTERAR UMA INFORMACAO ESPECIFICA
+ */
+app.get("/", (request, response) =>{
+  return response.json({
+    message: "Ola NLW 05!",
+  })
+})
+app.post("/", (request, response) => {
+  return response.json({message: "Usuario salvo com sucesso",})
+})
  app.listen(3333, () => console.log("Server running on port 3333"));
